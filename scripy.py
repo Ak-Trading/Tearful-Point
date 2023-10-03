de37a85c3141ba3273dc4ee0547439e676c3fcc6
Ahmed Elkady <ahelkady110@gmail.com>
2023-10-03 16:17:20 +0300

    delete the env

--- scripy.py
+++ scripy.py
@@ -1,32 +1,26 @@
 from ib_insync import *
-import argparse
+import asyncio
+import time
+commands=[]
 
+ib=IB()
+ib.connect('127.0.0.1',7497,0)    
 
-class IB:
-    ib=IB()
-    ib.connect('127.0.0.1',7497,0)
+class Strategy():
 
-class Strategy:
-
-    def __init__(self,string) -> None:
+    def __init__(self) -> None:
       
-        self.string=string
+        self.string=None
+        
         self.list_of_args = []
         self.dict_of_args = {}
-        self.get_list_args()
-        self.set_dict_args()
-        self.make_order()
-
     
     def get_list_args(self):
         self.list_of_args = self.string.split(' ')
 
     def update_quantity(self,contract):
         if self.dict_of_args["quantity"] == "ALL":
-            # sum all the quantity based on account salary
             account_details = IB.ib.accountSummary()
-
-            # Filter the account details to find the cash balance
             cash_balance = None
 
             for item in account_details:
@@ -34,9 +28,14 @@ class Strategy:
                     cash_balance = float(item.value)
                     break
 
-            IB.ib.qualifyContracts(contract)
-            market_data = IB.ib.reqMktData(contract)
-            IB.ib.sleep(2)
+            ib.qualifyContracts(contract)
+            market_data = ib.reqMktData(contract)
+            start_time = time.time()
+            while market_data == None:
+                continue
+            end_time = time.time()
+            print(f"Time taken: {end_time - start_time}")
+
             quantity = int(cash_balance / market_data.close)
             self.dict_of_args["quantity"]=quantity
         else:
@@ -45,16 +44,19 @@ class Strategy:
 
 
     def update_price(self,contract):
-       
         if "price" in self.dict_of_args.keys():
             try:
+                print(self.dict_of_args["price"])
                 if self.dict_of_args["price"][3] == '+':
-                    IB.ib.qualifyContracts(contract)
-                    market_data = IB.ib.reqMktData(contract)
-                    IB.ib.sleep(10)
+                    ib.qualifyContracts(contract)
+                    market_data = ib.reqMktData(contract)
+                    import math
+                    while math.isnan(market_data.bid) or math.isnan(market_data.ask):
+                        ib.sleep(0.1)
+                        continue
                     lst=self.dict_of_args["price"].split('+')
                     if lst[0] == "MID":
-                        self.dict_of_args["price"]=market_data.mid
+                        self.dict_of_args["price"]=(market_data.bid+market_data.ask)/2
                     elif lst[0] == "BID":
                         self.dict_of_args["price"]=market_data.bid
                     elif lst[0] == "ASK":
@@ -63,12 +65,16 @@ class Strategy:
                     self.dict_of_args["price"]+=float(lst[1])
                 
                 elif(self.dict_of_args["price"][3] == '-'):
-                    IB.ib.qualifyContracts(contract)
-                    market_data = IB.ib.reqMktData(contract)
-                    IB.ib.sleep(10)
+                    ib.qualifyContracts(contract)
+                    market_data = ib.reqMktData(contract)
+                    import math
+
+                    while math.isnan(market_data.bid) or math.isnan(market_data.ask):
+                        ib.sleep(0.1)
+                        continue
                     lst=self.dict_of_args["price"].split('-')
                     if lst[0] == "MID":
-                        self.dict_of_args["price"]=market_data.mid
+                        self.dict_of_args["price"]=(market_data.bid+market_data.ask)/2
                     elif lst[0] == "BID":
                         self.dict_of_args["price"]=market_data.bid
                     elif lst[0] == "ASK":
@@ -78,6 +84,8 @@ class Strategy:
                     self.dict_of_args["price"]=float(self.dict_of_args["price"])
             except Exception as e:
                 self.dict_of_args["price"]=float(self.dict_of_args["price"])
+
+            print(self.dict_of_args)
         
 
     def set_dict_args(self):
@@ -90,6 +98,11 @@ class Strategy:
         self.dict_of_args["quantity"] = self.list_of_args[1]
         self.dict_of_args["symbol"] = self.list_of_args[2]
         self.dict_of_args["order_type"] = self.list_of_args[3]
+
+        self.dict_of_args["symbol"]=self.dict_of_args["symbol"].upper()
+        self.dict_of_args["action"] = self.dict_of_args["action"].upper()
+        self.dict_of_args["order_type"] =self.dict_of_args["order_type"].upper()
+        self.dict_of_args["quantity"] =self.dict_of_args["quantity"].upper()
 
         if len(self.list_of_args) > 4:
 
@@ -116,6 +129,7 @@ class Strategy:
                             self.dict_of_args["catch"]=self.list_of_args[6]
                     else:
                         self.dict_of_args["catch"]=self.list_of_args[5]
+                        
 
 
               
@@ -130,10 +144,9 @@ class Strategy:
             else:
                 self.dict_of_args["price"]=self.list_of_args[4]
 
-        self.dict_of_args["symbol"]=self.dict_of_args["symbol"].upper()
-        self.dict_of_args["action"] = self.dict_of_args["action"].upper()
-        self.dict_of_args["order_type"] =self.dict_of_args["order_type"].upper()
-        self.dict_of_args["quantity"] =self.dict_of_args["quantity"].upper()
+        
+
+        print(self.dict_of_args)
 
             
 
@@ -149,7 +162,7 @@ class Strategy:
         else:
             contract.symbol = self.dict_of_args["symbol"]
             contract.exchange = "SMART"
-
+        
         self.update_price(contract)
         self.update_quantity(contract)
 
@@ -159,26 +172,60 @@ class Strategy:
         order.totalQuantity = int(self.dict_of_args["quantity"])
         if order.orderType == "LMT" or order.orderType == "TWAP":
             order.lmtPrice = self.dict_of_args["price"]
-        
-        if order.orderType == "TWAP":
-            order.lmtPrice = self.dict_of_args["price"]
 
-            print(order)
+            import datetime
 
+            # Get the current date and time
+            current_datetime = datetime.datetime.now()
+
+            # Format the date and time as 'YYYYMMDD-HH:mm:ss'
+            formatted_datetime = current_datetime.strftime('%Y%m%d-%H:%M:%S')
+            print(formatted_datetime)
+
+            # Format the combined date and time
+            new_formatted_datetime = formatted_datetime.split('-')[0] + '-' + self.dict_of_args['time']
+            print(new_formatted_datetime)
+            if order.orderType == "TWAP":
+                order.algoStrategy='Twap'
+                order.orderType='LMT'
+                if self.dict_of_args["catch"] != '':
+                    flag="True"
+                else:
+                    flag="False"
+                order.algoParams=[  TagValue(tag='endTime',value=self.dict_of_args['time']),
+                                    TagValue(tag='catchUp',value=flag)]
+                
+        print(order)
         try:
-            pass
-            IB.ib.placeOrder(contract,order)
+            x=ib.placeOrder(contract,order)
+            print(x)
         except Exception as e:
-            pass
-def handle_input(command):
-    strategy=Strategy(command)
+            print(e)
+        
+
+    def run(self,command):
+        self.string=command
+        self.get_list_args()
+        self.set_dict_args()
+        self.make_order()
+
+async def handle_input():
+    strategy=Strategy()
+    if len(commands) > 0:
+        for i in range(len(commands)):
+            strategy.run(commands[i])
+            commands.pop(0)
     
-           
+
+async def some_async_function():
+    await handle_input()
+
+        
+    
 if __name__ == "__main__":
 
-    while True :
-        
-        command = input("Enter The Statement:")
-        handle_input(command)
-
-    
+    while True:
+        command=input("Enter command: ")
+        commands.append(command)
+        asyncio.run(some_async_function())
+        print("Done")
