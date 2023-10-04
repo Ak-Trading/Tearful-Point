0d5e4aba6d3252ffc7e0d7fb1ce54fed0240ff87
Ahmed Elkady <ahelkady110@gmail.com>
2023-10-04 15:11:34 +0300

    version 2 (not stable)

--- script.py
+++ script.py
@@ -1,231 +1,244 @@
 from ib_insync import *
 import asyncio
 import time
-commands=[]
+import nest_asyncio
+nest_asyncio.apply()
 
-ib=IB()
-ib.connect('127.0.0.1',7497,0)    
+commands = []
 
-class Strategy():
-
+ib = IB()
+ib.connect("127.0.0.1", 7497, 0)
+class Strategy:
     def __init__(self) -> None:
-      
-        self.string=None
-        
+        self.string = None
         self.list_of_args = []
         self.dict_of_args = {}
+       
+        
+
+    def get_market_data(self,contract):
+        print("# get_market_data")
+        ib.qualifyContracts(contract)
+        market_data = ib.reqMktData(contract)
+        print(market_data)
+        import math
+        while math.isnan(market_data.bid) or math.isnan(market_data.ask):
+            ib.sleep(0.1)
+            print("sleep")
+            continue
+        print(market_data)
     
     def get_list_args(self):
-        self.list_of_args = self.string.split(' ')
+        self.list_of_args = self.string.split(" ")
+        self.list_of_args = list(map(lambda x: x.upper(), self.list_of_args))
+        print(self.list_of_args)
 
-    def update_quantity(self,contract):
+    def update_quantity(self, contract):
+        print("# update_quantity")
+        self.get_market_data(contract)
         if self.dict_of_args["quantity"] == "ALL":
-            account_details = IB.ib.accountSummary()
-            cash_balance = None
+            print("ALL")
+            try:
+                account_details = self.ib.accountSummary()
+                cash_balance = 1
 
-            for item in account_details:
-                if item.tag == 'TotalCashValue':
-                    cash_balance = float(item.value)
-                    break
+                for item in account_details:
+                    if item.tag == "TotalCashValue":
+                        cash_balance = float(item.value)
+                        break
+            
+                print(cash_balance)
+                self.get_market_data(contract)
 
-            ib.qualifyContracts(contract)
-            market_data = ib.reqMktData(contract)
-            start_time = time.time()
-            while market_data == None:
-                continue
-            end_time = time.time()
-            print(f"Time taken: {end_time - start_time}")
+                quantity = int(cash_balance / self.market_data.close)
+                self.dict_of_args["quantity"] = quantity
+            except Exception as e:
+                print(e)
 
-            quantity = int(cash_balance / market_data.close)
-            self.dict_of_args["quantity"]=quantity
         else:
-            self.dict_of_args["quantity"]=int(self.dict_of_args["quantity"])
+            self.dict_of_args["quantity"] = int(self.dict_of_args["quantity"])
+        print(self.dict_of_args)
 
-
-
-    def update_price(self,contract):
+    def update_price(self, contract):
+        print("# update_price")
+        print(self.dict_of_args)
         if "price" in self.dict_of_args.keys():
             try:
                 print(self.dict_of_args["price"])
-                if self.dict_of_args["price"][3] == '+':
-                    ib.qualifyContracts(contract)
-                    market_data = ib.reqMktData(contract)
-                    import math
-                    while math.isnan(market_data.bid) or math.isnan(market_data.ask):
-                        ib.sleep(0.1)
-                        continue
-                    lst=self.dict_of_args["price"].split('+')
+                if self.dict_of_args["price"][3] == "+":
+                    print(self.market_data)
+                    lst = self.dict_of_args["price"].split("+")
                     if lst[0] == "MID":
-                        self.dict_of_args["price"]=(market_data.bid+market_data.ask)/2
+                        self.dict_of_args["price"] = (
+                            self.market_data.bid + self.market_data.ask
+                        ) / 2
                     elif lst[0] == "BID":
-                        self.dict_of_args["price"]=market_data.bid
+                        self.dict_of_args["price"] = self.market_data.bid
                     elif lst[0] == "ASK":
-                        self.dict_of_args["price"]=market_data.ask
+                        self.dict_of_args["price"] = self.market_data.ask
 
-                    self.dict_of_args["price"]+=float(lst[1])
-                
-                elif(self.dict_of_args["price"][3] == '-'):
-                    ib.qualifyContracts(contract)
-                    market_data = ib.reqMktData(contract)
-                    import math
+                    self.dict_of_args["price"] += float(lst[1])
 
-                    while math.isnan(market_data.bid) or math.isnan(market_data.ask):
-                        ib.sleep(0.1)
-                        continue
-                    lst=self.dict_of_args["price"].split('-')
+                elif self.dict_of_args["price"][3] == "-":
+                    
+                    lst = self.dict_of_args["price"].split("-")
                     if lst[0] == "MID":
-                        self.dict_of_args["price"]=(market_data.bid+market_data.ask)/2
+                        self.dict_of_args["price"] = (
+                            self.market_data.bid + self.market_data.ask
+                        ) / 2
                     elif lst[0] == "BID":
-                        self.dict_of_args["price"]=market_data.bid
+                        self.dict_of_args["price"] = self.market_data.bid
                     elif lst[0] == "ASK":
-                        self.dict_of_args["price"]=market_data.ask
-                    self.dict_of_args["price"]-=float(lst[1])
+                        self.dict_of_args["price"] = self.market_data.ask
+                    self.dict_of_args["price"] -= float(lst[1])
                 else:
-                    self.dict_of_args["price"]=float(self.dict_of_args["price"])
+                    self.dict_of_args["price"] = float(self.dict_of_args["price"])
             except Exception as e:
-                self.dict_of_args["price"]=float(self.dict_of_args["price"])
-
+                self.dict_of_args["price"] = float(self.dict_of_args["price"])
+                pass
             print(self.dict_of_args)
-        
 
     def set_dict_args(self):
-
         if len(self.list_of_args) < 4:
             raise ValueError("Not enough arguments")
-        
-        
-        self.dict_of_args["action"] = self.list_of_args[0]
-        self.dict_of_args["quantity"] = self.list_of_args[1]
-        self.dict_of_args["symbol"] = self.list_of_args[2]
-        self.dict_of_args["order_type"] = self.list_of_args[3]
 
-        self.dict_of_args["symbol"]=self.dict_of_args["symbol"].upper()
-        self.dict_of_args["action"] = self.dict_of_args["action"].upper()
-        self.dict_of_args["order_type"] =self.dict_of_args["order_type"].upper()
-        self.dict_of_args["quantity"] =self.dict_of_args["quantity"].upper()
-
+        self.dict_of_args["action"] = self.list_of_args[0].upper()
+        self.dict_of_args["quantity"] = self.list_of_args[1].upper()
+        self.dict_of_args["symbol"] = self.list_of_args[2].upper()
+        self.dict_of_args["order_type"] = self.list_of_args[3].upper()
+        print(self.list_of_args)
+        print(self.dict_of_args)
         if len(self.list_of_args) > 4:
-
             if self.dict_of_args["order_type"] == "TWAP":
-                self.dict_of_args["price"]=self.list_of_args[4]
+                self.dict_of_args["price"] = self.list_of_args[4]
 
-                if self.dict_of_args["price"] in ["MID","BID","ASK"]:
+                if self.dict_of_args["price"] in ["MID", "BID", "ASK"]:
                     if self.list_of_args[5][0] == "+" or self.list_of_args[5][0] == "-":
-                        self.dict_of_args["price"]+=self.list_of_args[5]
+                        self.dict_of_args["price"] += self.list_of_args[5]
 
-                        if self.list_of_args[6] != "catch" and self.list_of_args[7] != "catch-up":
-                            self.dict_of_args["time"]=self.list_of_args[6]
+                        if (
+                            self.list_of_args[6] != "catch"
+                            and self.list_of_args[7] != "catch-up"
+                        ):
+                            self.dict_of_args["time"] = self.list_of_args[6]
 
-                            self.dict_of_args["catch"]=self.list_of_args[7]
+                            self.dict_of_args["catch"] = self.list_of_args[7]
                         else:
-                            self.dict_of_args["catch"]=self.list_of_args[6]
+                            self.dict_of_args["catch"] = self.list_of_args[6]
                     else:
-                        self.dict_of_args["catch"]=self.list_of_args[5]
+                        self.dict_of_args["catch"] = self.list_of_args[5]
 
                 else:
-                    if self.list_of_args[5] != "catch" and self.list_of_args[5] != "catch-up":
-                            self.dict_of_args["time"]=self.list_of_args[5]
+                    if len(self.list_of_args) > 5:
+                        if (
+                            self.list_of_args[5] != "catch"
+                            and self.list_of_args[5] != "catch-up"
+                        ):
+                            self.dict_of_args["time"] = self.list_of_args[5]
+                            self.dict_of_args["catch"] = ""
+                            if len(self.list_of_args) == 7:
+                                self.dict_of_args["catch"] = self.list_of_args[6]
+                        else:
+                            self.dict_of_args["catch"] = self.list_of_args[5]
+                            self.dict_of_args["time"] = ""
 
-                            self.dict_of_args["catch"]=self.list_of_args[6]
                     else:
-                        self.dict_of_args["catch"]=self.list_of_args[5]
-                        
+                        self.dict_of_args["catch"] = ""
+                        self.dict_of_args["time"] = ""
 
+            elif self.dict_of_args["order_type"] == "LMT":
+                self.dict_of_args["price"] = self.list_of_args[4]
 
-              
-            elif self.dict_of_args["order_type"] =="LMT":
-                self.dict_of_args["price"]=self.list_of_args[4]
-                
-                if self.dict_of_args["price"] in ["MID","BID","ASK"]:
+                if self.dict_of_args["price"] in ["MID", "BID", "ASK"]:
                     if self.list_of_args[5][0] == "+" or self.list_of_args[5][0] == "-":
-                        self.dict_of_args["price"]+=self.list_of_args[5]
-                        
+                        self.dict_of_args["price"] += self.list_of_args[5]
 
             else:
-                self.dict_of_args["price"]=self.list_of_args[4]
-
-        
+                self.dict_of_args["price"] = self.list_of_args[4]
 
         print(self.dict_of_args)
-
-            
 
     def make_order(self):
         contract = Contract()
         contract.symbol = self.dict_of_args["symbol"]
         contract.secType = "STK"
         contract.currency = "USD"
-        if len(self.dict_of_args["symbol"].split(':')) > 1:
-            contract.symbol = self.dict_of_args["symbol"].split(':')[1]
-            contract.exchange = self.dict_of_args["symbol"].split(':')[0]
+
+        if len(self.dict_of_args["symbol"].split(":")) > 1:
+            contract.symbol = self.dict_of_args["symbol"].split(":")[1]
+            contract.exchange = self.dict_of_args["symbol"].split(":")[0]
             contract.currency = "EUR"
         else:
             contract.symbol = self.dict_of_args["symbol"]
             contract.exchange = "SMART"
-        
-        self.update_price(contract)
+
         self.update_quantity(contract)
 
-        order=Order()
+        order = Order()
         order.action = self.dict_of_args["action"]
         order.orderType = self.dict_of_args["order_type"]
         order.totalQuantity = int(self.dict_of_args["quantity"])
         if order.orderType == "LMT" or order.orderType == "TWAP":
+            self.update_price(contract)
+
             order.lmtPrice = self.dict_of_args["price"]
-
-            import datetime
-
-            # Get the current date and time
-            current_datetime = datetime.datetime.now()
-
-            # Format the date and time as 'YYYYMMDD-HH:mm:ss'
-            formatted_datetime = current_datetime.strftime('%Y%m%d-%H:%M:%S')
-            print(formatted_datetime)
-
-            # Format the combined date and time
-            new_formatted_datetime = formatted_datetime.split('-')[0] + '-' + self.dict_of_args['time']
-            print(new_formatted_datetime)
             if order.orderType == "TWAP":
-                order.algoStrategy='Twap'
-                order.orderType='LMT'
-                if self.dict_of_args["catch"] != '':
-                    flag="True"
+                order.algoStrategy = "Twap"
+                order.orderType = "LMT"
+                order.algoParams = []
+                if self.dict_of_args["catch"] != "":
+                    order.algoParams.append(TagValue(tag="catchUp", value="True"))
                 else:
-                    flag="False"
-                order.algoParams=[  TagValue(tag='endTime',value=self.dict_of_args['time']),
-                                    TagValue(tag='catchUp',value=flag)]
-                
+                    order.algoParams.append(TagValue(tag="catchUp", value="False"))
+
+                if self.dict_of_args["time"] != "":
+                    from datetime import datetime
+
+                    order.algoParams.append(
+                        TagValue(
+                            tag="startTime", value=datetime.now().strftime("%H:%M:%S")
+                        )
+                    )
+                    order.algoParams.append(
+                        TagValue(tag="endTime", value=self.dict_of_args["time"])
+                    )
+                    print(order.algoParams)
+
+                else:
+                    from datetime import datetime
+
+                    order.algoParams.append(
+                        TagValue(
+                            tag="startTime", value=datetime.now().strftime("%H:%M:%S")
+                        )
+                    )
+                    order.algoParams.append(TagValue(tag="endTime", value="22:00:00"))
+
         print(order)
         try:
-            x=ib.placeOrder(contract,order)
-            print(x)
+            x = ib.placeOrder(contract, order)
         except Exception as e:
             print(e)
-        
 
-    def run(self,command):
-        self.string=command
+    def run(self, command):
+        self.string = command
         self.get_list_args()
         self.set_dict_args()
         self.make_order()
 
-async def handle_input():
-    strategy=Strategy()
-    if len(commands) > 0:
-        for i in range(len(commands)):
-            strategy.run(commands[i])
-            commands.pop(0)
-    
 
-async def some_async_function():
-    await handle_input()
+async def handle_input(commands):
+    strategy = Strategy()
+    strategy.run(commands)
 
-        
-    
+
+def some_async_function(command):
+    asyncio.run(handle_input(command))
+
+
+
 if __name__ == "__main__":
-
     while True:
-        command=input("Enter command: ")
-        commands.append(command)
-        asyncio.run(some_async_function())
+        command = input("Enter command: ")
+        some_async_function(command)
         print("Done")
--- telegram.py
+++ telegram.py
@@ -1,28 +1,35 @@
 import json
+import threading
+import asyncio
+import tracemalloc
+
 import telebot
 import script
-from threading import Thread
-import asyncio
 
-class BaseBot():
+# tracemalloc.start()
+# asyncio.set_event_loop_policy(asyncio.DefaultEventLoopPolicy())
+
+commands = []
+
+
+class BaseBot:
     """
     super class for telegram bot
-    
+
     attributes:
         symbols: a list of symbols to be monitored
         bot: a telebot object
     """
-    
+
     single_bot_instance = None
-    
+
     def __init__(self):
         """
-        initialize the class, read the config file and connect to the bot        
+        initialize the class, read the config file and connect to the bot
         """
         self.read_config()
         self.bot
-        
-        
+
     @property
     def bot(self):
         """
@@ -30,70 +37,65 @@ class BaseBot():
         """
         if BaseBot.single_bot_instance is None:
             try:
-                BaseBot.single_bot_instance =telebot.TeleBot(self.token)
+                BaseBot.single_bot_instance = telebot.TeleBot(self.token)
             except Exception:
-                file_path="config.json"
+                file_path = "config.json"
                 with open(file_path) as json_data_file:
                     self.data = json.load(json_data_file)
 
-                self.data["telegram_enable"]=False
+                self.data["telegram_enable"] = False
                 with open(file_path, 'w') as json_file:
                     json.dump(self.data, json_file, indent=4)
 
                     return None
-    
 
-        return BaseBot.single_bot_instance       
-    
+        return BaseBot.single_bot_instance
+
     def read_config(self):
         """
         read the config file
         """
-        file_path="config.json"
+        file_path = "config.json"
         with open(file_path) as json_data_file:
             self.data = json.load(json_data_file)
-            
+
         self.token = self.data["token"]
         self.chat_id = self.data["chat_id"]
-       
-        
-   
-        
-  
+
+
 class TelegramController(BaseBot):
-    
-    """_summary_
-    this class is used to listen to the user and control the system
-   
     """
-    
+    this class is used to listen to the user and control the system
+    """
+
     def __init__(self):
-        
-        super().__init__()  
-        self.commands=[]
-        
-        
-    def listener_start(self): 
-         
+        super().__init__()
+        self.commands = []
+
+    def listener_start(self):
         """
         listen to the user and control the system
         """
         @self.bot.message_handler(func=lambda message: True)
         def handle_start(message):
-            print(message.text)  
+            print(message.text)
+            wait = threading.Thread(target=script.some_async_function(message.text))
+            wait.start()
             script.commands.append(message.text)
-            asyncio.run(script.handle_input())
-
+            
     def run(self):
         self.listener_start()
         self.bot.infinity_polling()
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     TelegramBotcontroller = TelegramController()
     TelegramBotcontroller.run()
-    
-    
-    
-    
-                           
+    # thread_1 = threading.Thread(target=start_async_function)
+    # thread_1.start()
+    # print("started")
+    # TelegramBotcontroller.run()
+    # thread_1.join()
+    # TelegramBotcontroller.bot.infinity_polling()
+    # script.handle_input()
+
