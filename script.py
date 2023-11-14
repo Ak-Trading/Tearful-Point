2c3e4eb8f3b1345a75679467a3d34eac5c26068b
KyRillos BoshRa <kyrillos@aktrading.io>
2023-11-14 19:44:27 +0200

    add options support

--- script.py
+++ script.py
@@ -1,15 +1,20 @@
+import calendar
+import datetime
 from ib_insync import *
 import asyncio
 import time
 import nest_asyncio
+
 nest_asyncio.apply()
 import threading
+
 util.patchAsyncio()
 
 commands = []
 
+
 class Strategy(threading.Thread):
-    def __init__(self,*args, **kwargs) -> None:
+    def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
         self.string = None
         self.list_of_args = []
@@ -17,25 +22,57 @@ class Strategy(threading.Thread):
         self.market_data = None
         self.ib = IB()
         self.ib.connect("127.0.0.1", 7497, 0)
-        
 
-    def get_market_data(self,contract):
-        
+    def get_market_data(self, contract):
         market_data = self.ib.reqMktData(contract)
         import math
+
         while math.isnan(market_data.bid) or math.isnan(market_data.ask):
             self.ib.sleep(0.1)
             continue
         self.market_data = market_data
         self.ib.cancelMktData(contract)
-    
+
+    def update_mid(self, contract):
+        market_data = self.ib.reqMktData(contract)
+        import math
+
+        while math.isnan(market_data.bid):
+            self.ib.sleep(0.1)
+            continue
+        self.market_data = market_data
+        self.ib.cancelMktData(contract)
+        self.dict_of_args["price"] = round(self.market_data.midpoint(), 2)
+
+    def update_bid(self, contract):
+        market_data = self.ib.reqMktData(contract)
+        import math
+
+        while math.isnan(market_data.bid):
+            self.ib.sleep(0.1)
+            continue
+        self.market_data = market_data
+        self.ib.cancelMktData(contract)
+        self.dict_of_args["price"] = self.market_data.bid
+
+    def update_ask(self, contract):
+        market_data = self.ib.reqMktData(contract)
+        import math
+
+        while math.isnan(market_data.bid):
+            self.ib.sleep(0.1)
+            continue
+        self.market_data = market_data
+        self.ib.cancelMktData(contract)
+        self.dict_of_args["price"] = self.market_data.ask
+
     def get_list_args(self):
         self.list_of_args = self.string.split(" ")
         self.list_of_args = list(map(lambda x: x.upper(), self.list_of_args))
 
     def update_quantity(self, contract):
         self.get_market_data(contract)
-        
+
         if self.dict_of_args["quantity"] == "ALL":
             try:
                 account_details = self.ib.accountSummary()
@@ -46,7 +83,7 @@ class Strategy(threading.Thread):
                         break
                 quantity = int(cash_balance / self.market_data.close)
                 self.dict_of_args["quantity"] = quantity
-                
+
             except Exception as e:
                 print(e)
 
@@ -70,7 +107,6 @@ class Strategy(threading.Thread):
                     self.dict_of_args["price"] += float(lst[1])
 
                 elif self.dict_of_args["price"][3] == "-":
-                    
                     lst = self.dict_of_args["price"].split("-")
                     if lst[0] == "MID":
                         self.dict_of_args["price"] = (
@@ -87,7 +123,43 @@ class Strategy(threading.Thread):
                 self.dict_of_args["price"] = float(self.dict_of_args["price"])
                 pass
 
+    def set_dict_args_options(self):
+        self.dict_of_args["action"] = self.list_of_args[0]
+        self.dict_of_args["quantity"] = self.list_of_args[1]
+        if ":" in self.list_of_args[2]:
+            self.dict_of_args["symbol"] = self.list_of_args[2].split(":")[0]
+            self.dict_of_args["exchange"] = self.list_of_args[2].split(":")[1]
+        else:
+            self.dict_of_args["symbol"] = self.list_of_args[2]
+            self.dict_of_args["exchange"] = "SMART"
+        if len(self.list_of_args[3]) == 3:
+            self.dict_of_args["expiration"] = self.get_third_friday(
+                self.list_of_args[3]
+            )
+        else:
+            self.dict_of_args["expiration"] = self.get_option_date(
+                self.list_of_args[3][:3], self.list_of_args[3][-2:]
+            )
+        self.dict_of_args["right"] = self.list_of_args[4]
+        self.dict_of_args["strike"] = float(self.list_of_args[5])
+        self.dict_of_args["order_type"] = self.list_of_args[6]
+        if self.dict_of_args["order_type"] == "LMT":
+            self.dict_of_args["price"] = self.list_of_args[7]
+            if "+" in self.dict_of_args["price"]:
+                lst = self.dict_of_args["price"].split("+")
+                self.dict_of_args["price"] = lst[0]
+                self.dict_of_args["inc"] = float(lst[1])
+            elif "-" in self.dict_of_args["price"]:
+                lst = self.dict_of_args["price"].split("-")
+                self.dict_of_args["price"] = lst[0]
+                self.dict_of_args["inc"] = -float(lst[1])
+            else:
+                self.dict_of_args["inc"] = 0
+
     def set_dict_args(self):
+        if "C" in self.list_of_args or "P" in self.list_of_args:
+            self.set_dict_args_options()
+            return
         if len(self.list_of_args) < 4:
             raise ValueError("Not enough arguments")
 
@@ -95,7 +167,7 @@ class Strategy(threading.Thread):
         self.dict_of_args["quantity"] = self.list_of_args[1].upper()
         self.dict_of_args["symbol"] = self.list_of_args[2].upper()
         self.dict_of_args["order_type"] = self.list_of_args[3].upper()
-        
+
         if len(self.list_of_args) > 4:
             if self.dict_of_args["order_type"] == "TWAP":
                 self.dict_of_args["price"] = self.list_of_args[4]
@@ -144,7 +216,6 @@ class Strategy(threading.Thread):
             else:
                 self.dict_of_args["price"] = self.list_of_args[4]
 
-
     def make_order(self):
         contract = Contract()
         contract.symbol = self.dict_of_args["symbol"]
@@ -167,7 +238,7 @@ class Strategy(threading.Thread):
         order.totalQuantity = int(self.dict_of_args["quantity"])
         if order.orderType == "LMT" or order.orderType == "TWAP":
             self.update_price(contract)
-            order.lmtPrice = round(self.dict_of_args["price"],2)
+            order.lmtPrice = round(self.dict_of_args["price"], 2)
             if order.orderType == "TWAP":
                 order.algoStrategy = "Twap"
                 order.orderType = "LMT"
@@ -199,18 +270,115 @@ class Strategy(threading.Thread):
                     )
                     order.algoParams.append(TagValue(tag="endTime", value="22:00:00"))
 
-    
         try:
             self.ib.placeOrder(contract, order)
             self.ib.sleep(5)
         except Exception as e:
             print(e)
 
+    def get_third_friday(self, month, next_year=False):
+        month_dict = {
+            "JAN": 1,
+            "FEB": 2,
+            "MAR": 3,
+            "APR": 4,
+            "MAY": 5,
+            "JUN": 6,
+            "JUL": 7,
+            "AUG": 8,
+            "SEP": 9,
+            "OCT": 10,
+            "NOV": 11,
+            "DEC": 12,
+        }
+        year = calendar.datetime.date.today().year
+        if next_year:
+            year += 1
+
+        month_num = month_dict[month.upper()]
+        cal = calendar.monthcalendar(year, month_num)
+        first_row = cal[0]
+        if first_row[calendar.FRIDAY]:
+            third_friday = cal[2][calendar.FRIDAY]
+        else:
+            third_friday = cal[3][calendar.FRIDAY]
+        if datetime.datetime.now() > datetime.datetime(year, month_num, third_friday):
+            return self.get_third_friday(month, True)
+        return f"{year}{month_num:02d}{third_friday:02d}"
+
+    def get_option_date(self, month, day):
+        month_dict = {
+            "JAN": 1,
+            "FEB": 2,
+            "MAR": 3,
+            "APR": 4,
+            "MAY": 5,
+            "JUN": 6,
+            "JUL": 7,
+            "AUG": 8,
+            "SEP": 9,
+            "OCT": 10,
+            "NOV": 11,
+            "DEC": 12,
+        }
+        date = datetime.datetime.now().replace(month=month_dict[month], day=int(day))
+        if date < datetime.datetime.now():
+            date = date.replace(year=date.year + 1)
+        return date.strftime("%Y%m%d")
+
+    def make_options_order(self):
+        contract = Option(
+            self.dict_of_args["symbol"],
+            self.dict_of_args["expiration"],
+            self.dict_of_args["strike"],
+            self.dict_of_args["right"],
+            self.dict_of_args["exchange"],
+            currency="USD",
+        )
+        self.ib.qualifyContracts(contract)
+        all_q = False
+        if self.dict_of_args["quantity"] == "ALL":
+            all_q = True
+        self.update_quantity(contract)
+        if all_q:
+            self.dict_of_args["quantity"] //= int(contract.multiplier)
+        if self.dict_of_args["order_type"] == "MKT":
+            self.ib.placeOrder(
+                contract,
+                MarketOrder(self.dict_of_args["action"], self.dict_of_args["quantity"]),
+            )
+        elif self.dict_of_args["order_type"] == "LMT":
+            if self.dict_of_args["price"] == "MID":
+                self.update_mid(contract)
+            if self.dict_of_args["price"] == "BID":
+                self.update_bid(contract)
+            if self.dict_of_args["price"] == "ASK":
+                self.update_ask(contract)
+
+            lmt_price = float(self.dict_of_args["price"]) + self.dict_of_args["inc"]
+            self.ib.placeOrder(
+                contract,
+                LimitOrder(
+                    self.dict_of_args["action"],
+                    self.dict_of_args["quantity"],
+                    lmt_price,
+                ),
+            )
+
+        self.ib.sleep(1)
+
+    def get_trade_exec(self):
+        if "right" in self.dict_of_args:
+            return self.make_options_order
+        else:
+            return self.make_order
+
     def run(self, command):
         self.string = command
         self.get_list_args()
         self.set_dict_args()
-        self.make_order()
+        trade = self.get_trade_exec()
+        trade()
         print("#################")
 
 
@@ -225,8 +393,5 @@ def handle_input():
             except Exception as e:
                 print(e)
                 continue
-        
+
         commands.clear()
-        
-        
-        
